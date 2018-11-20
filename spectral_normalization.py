7609eb1e2ccf42b30b461e47106feb9472dea87a
Barış Bayraktar <baris4111@gmail.com>
2018-11-20 23:21:25 +0300

    sngan + dcgan-like generator and discriminator

--- spectral_normalization.py
+++ spectral_normalization.py
@@ -1,3 +1,66 @@
-from torch import nn
+import torch
+from torch.optim.optimizer import Optimizer, required
+
+from torch.autograd import Variable
 import torch.nn.functional as F
-print("5")
+from torch import nn
+from torch import Tensor
+from torch.nn import Parameter
+
+
+def l2_normalization(v, eps=1e-12):
+    return v / (v.norm() + eps)
+
+
+class SpectralNorm(nn.Module):
+    def __init__(self, module, name='weight', power_iterations=1):
+        super(SpectralNorm, self).__init__()
+        self.module = module
+        self.name = name
+        self.power_iterations = power_iterations
+        
+        if not self._made_params():
+            self._make_params()
+
+    def _update_u_v(self):
+        u = getattr(self.module, self.name + "_u")
+        v = getattr(self.module, self.name + "_v")
+        w = getattr(self.module, self.name + "_w")
+
+        height = w.data.shape[0]
+        for _ in range(self.power_iterations):
+            v.data = l2_normalization(torch.mv(torch.t(w.view(height, -1).data), u.data))
+            u.data = l2_normalization(torch.mv(w.view(height, -1).data, v.data))
+
+            sigma = u.dot(w.view(height, -1).mv(v))
+            setattr(self.module, self.name, w / sigma.expand_as(w))
+
+    def _made_params(self):
+        try:
+            u = getattr(self.module, self.name + "_u")
+            v = getattr(self.module, self.name + "_v")
+            w = getattr(self.module, self.name + "_w")
+            return True
+        except AttributeError:
+            return False
+
+    def _make_params(self):
+        w = getattr(self.module, self.name)
+        height = w.data.shape[0]
+        width = w.view(height, -1).data.shape[1]
+
+        u = nn.Parameter(w.data.new(height).normal_(0, 1), requires_grad=False)
+        v = nn.Parameter(w.data.new(width).normal_(0, 1), requires_grad=False)
+        u.data = l2_normalization(u.data)
+        v.data = l2_normalization(v.data)
+        w = nn.Parameter(w.data)
+
+        del self.module._parameters[self.name]
+
+        self.module.register_parameter(self.name + "_u", u)
+        self.module.register_parameter(self.name + "_v", v)
+        self.module.register_parameter(self.name + "_w", w)
+
+    def forward(self, *args):
+        self._update_u_v()
+        return self.module.forward(*args)
