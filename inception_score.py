dedae0fe11e52dafd5c3cc796e33ac34fe58ffb9
bugrabaran <orhunbugra95@gmail.com>
2019-01-09 14:28:58 +0300

    Inception score & some tweaks

---
+++ inception_score.py
@@ -0,0 +1,101 @@
+import torch
+from torch import nn
+from torch.autograd import Variable
+from torch.nn import functional as F
+import torch.utils.data
+
+from torchvision.models.inception import inception_v3
+
+import numpy as np
+from scipy.stats import entropy
+
+def inception_score(imgs, cuda=True, batch_size=32, resize=False, splits=1):
+    # Number of generated images
+    N = len(imgs)
+
+    assert batch_size > 0
+    assert N > batch_size
+
+    # Set up dtype
+    if cuda:
+        dtype = torch.cuda.FloatTensor
+    else:
+        if torch.cuda.is_available():
+            print("You have a CUDA device, set cuda=True")
+        dtype = torch.FloatTensor
+
+    # Set up dataloader
+    dataloader = torch.utils.data.DataLoader(imgs, batch_size=batch_size)   
+
+    # Load inception model
+    inception_model = inception_v3(pretrained=True, transform_input=False).type(dtype)
+    inception_model.eval();
+    def get_pred(x):
+        if resize:
+            x = F.interpolate(x, size=(299,299), mode='bilinear')
+        x = inception_model(x)
+        return F.softmax(input=x,dim=1).data.cpu().numpy()
+
+    # Get predictions, Inception network is trained on dataset with 1000 classes
+    preds = np.zeros((N, 1000))
+
+    for i, batch in enumerate(dataloader, 0):
+        batch = batch.type(dtype)
+        batchv = Variable(batch)
+        batch_size_i = batch.size()[0]
+
+        preds[i*batch_size:i*batch_size + batch_size_i] = get_pred(batchv)
+
+    # Now compute the mean kl-div
+    split_scores = []
+
+    for k in range(splits):
+        part = preds[k * (N // splits): (k+1) * (N // splits), :]
+        py = np.mean(part, axis=0)
+        scores = []
+        for i in range(part.shape[0]):
+            pyx = part[i, :]
+            scores.append(entropy(pyx, py))
+        split_scores.append(np.exp(np.mean(scores)))
+
+    return np.mean(split_scores), np.std(split_scores)
+
+
+"""
+##################################################################################################################
+#########################   This is used for testing inception score on original Cifar10 images     ##############
+##################################################################################################################
+if __name__ == '__main__':
+    class IgnoreLabelDataset(torch.utils.data.Dataset):
+        def __init__(self, orig):
+            self.orig = orig
+
+        def __getitem__(self, index):
+            return self.orig[index][0]
+
+        def __getNumberOfItems__(self, howMany):
+            data = torch.zeros([howMany,3,32,32], dtype = torch.float32)
+            for i in range(howMany-1):
+                data[i] = self.__getitem__(i)
+            return data
+                
+        def __len__(self):
+            return len(self.orig)
+
+    import torchvision.datasets as dset
+    import torchvision.transforms as transforms
+
+    cifar = dset.CIFAR10(root='data/', download=False,
+                             transform=transforms.Compose([
+                                 transforms.Scale(32),
+                                 transforms.ToTensor(),
+                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                             ])
+    )
+
+    IgnoreLabelDataset(cifar)
+
+    print ("Calculating Inception Score...")
+    print("cifar.size = ", IgnoreLabelDataset(cifar).__getNumberOfItems__(500).size())
+print (inception_score((cifar).__getNumberOfItems__(500), cuda=False, batch_size=32, resize=True, splits=10))
+"""
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from torch.optim.lr_scheduler import ExponentialLR
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 import model
-
+from inception_score import inception_score
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
@@ -23,35 +23,12 @@ parser.add_argument('--loss', type=str, default='hinge')
 parser.add_argument('--checkpoint_dir', type=str, default='checkpoints')
 
 parser.add_argument('--model', type=str, default='dcgan')
-
+parser.add_argument('--pretrained', type=str, default="False")
+parser.add_argument('--cuda_avail', type=str, default="True")
 args = parser.parse_args()
-
-loader = torch.utils.data.DataLoader(
-    datasets.CIFAR10('../data/', train=True, download=True,
-        transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])),
-        batch_size=args.batch_size, shuffle=True, num_workers=1, pin_memory=True)
-
 Z_dim = 128
 #number of updates to discriminator for every update to generator 
 disc_iters = 5
-
-if args.model == 'resnet':
-    discriminator = model_resnet.Discriminator().cuda()
-    generator = model_resnet.Generator(Z_dim).cuda()
-else:
-    discriminator = model.Discriminator().cuda()
-    generator = model.Generator(Z_dim).cuda()
-
-# because the spectral normalization module creates parameters that don't require gradients (u and v), we don't want to 
-# optimize these using sgd. We only let the optimizer operate on parameters that _do_ require gradients
-optim_disc = optim.Adam(filter(lambda p: p.requires_grad, discriminator.parameters()), lr=args.lr, betas=(0.0,0.9))
-optim_gen  = optim.Adam(generator.parameters(), lr=args.lr, betas=(0.0,0.9))
-
-# use an exponentially decaying learning rate
-scheduler_d = optim.lr_scheduler.ExponentialLR(optim_disc, gamma=0.99)
-scheduler_g = optim.lr_scheduler.ExponentialLR(optim_gen, gamma=0.99)
 
 def train(epoch):
     for batch_idx, (data, target) in enumerate(loader):
@@ -91,7 +68,6 @@ def train(epoch):
     scheduler_d.step()
     scheduler_g.step()
 
-fixed_z = Variable(torch.randn(args.batch_size, Z_dim).cuda())
 def evaluate(epoch):
 
     samples = generator(fixed_z).cpu().data.numpy()[:64]
@@ -114,11 +90,65 @@ def evaluate(epoch):
 
     plt.savefig('out/{}.png'.format(str(epoch).zfill(3)), bbox_inches='tight')
     plt.close(fig)
+def load_pretrained(model_type, cuda_avail):
+    if args.model == "resnet":
+        if args.cuda_avail == "True":
+            discriminator = model_resnet.Discriminator().cuda()
+            generator = model_resnet.Generator(Z_dim).cuda()
+            discriminator.load_state_dict(torch.load('checkpoints/disc_last', map_location=lambda storage, loc: storage))
+            generator.load_state_dict(torch.load('checkpoints/gen_last', map_location=lambda storage, loc: storage))
+        else:
+            discriminator = model_resnet.Discriminator()
+            generator = model_resnet.Generator(Z_dim)
+            discriminator.load_state_dict(torch.load('checkpoints/disc_last', map_location=lambda storage, loc: storage))
+            generator.load_state_dict(torch.load('checkpoints/gen_last', map_location=lambda storage, loc: storage))
+
+    else:
+        if args.cuda_avail == "True":
+            discriminator = model.Discriminator().cuda()
+            generator = model.Generator(Z_dim).cuda()
+            discriminator.load_state_dict(torch.load(args.checkpoint_dir + '/disc_last'))
+            generator.load_state_dict(torch.load(args.checkpoint_dir + '/gen_last'))
+        else:
+            discriminator = model.Discriminator()
+            generator = model.Generator(Z_dim)
+            discriminator.load_state_dict(torch.load(args.checkpoint_dir + '/disc_last', map_location=lambda storage, loc: storage))
+            generator.load_state_dict(torch.load(args.checkpoint_dir + '/gen_last', map_location=lambda storage, loc: storage))
+
+    return generator, discriminator        
 
 os.makedirs(args.checkpoint_dir, exist_ok=True)
+fixed_z = Variable(torch.randn(50000, Z_dim))
+if args.pretrained == "True":
+    generator, _ = load_pretrained(args.model, args.cuda_avail)
+    samples = generator(fixed_z).cpu().data.numpy()
+    print("Calculating inception_score ... mean %s, deviation %s"%(inception_score(samples, cuda = False if args.cuda_avail == "False" else True, batch_size = args.batch_size // 4, resize = True, splits = 10)))                
 
-for epoch in range(2000):
-    train(epoch)
-    evaluate(epoch)
-    torch.save(discriminator.state_dict(), os.path.join(args.checkpoint_dir, 'disc_{}'.format(epoch)))
-    torch.save(generator.state_dict(), os.path.join(args.checkpoint_dir, 'gen_{}'.format(epoch)))
+else:   # Here we assume cuda is a must for training 
+    loader = torch.utils.data.DataLoader(
+    datasets.CIFAR10('data/', train=True, download=False,
+        transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])),
+        batch_size=args.batch_size, shuffle=True, num_workers=1, pin_memory=True)
+    if args.model == 'resnet':
+        discriminator = model_resnet.Discriminator().cuda()
+        generator = model_resnet.Generator(Z_dim).cuda()
+    else:
+        discriminator = model.Discriminator().cuda()
+        generator = model.Generator(Z_dim).cuda()
+
+    # because the spectral normalization module creates parameters that don't require gradients (u and v), we don't want to 
+    # optimize these using sgd. We only let the optimizer operate on parameters that _do_ require gradients
+    optim_disc = optim.Adam(filter(lambda p: p.requires_grad, discriminator.parameters()), lr=args.lr, betas=(0.0,0.9))
+    optim_gen  = optim.Adam(generator.parameters(), lr=args.lr, betas=(0.0,0.9))
+
+    # use an exponentially decaying learning rate
+    scheduler_d = optim.lr_scheduler.ExponentialLR(optim_disc, gamma=0.99)
+    scheduler_g = optim.lr_scheduler.ExponentialLR(optim_gen, gamma=0.99)
+    fixed_z = Variable(torch.randn(args.batch_size, Z_dim).cuda())
+    for epoch in range(2000):
+        train(epoch)
+        evaluate(epoch)
+        torch.save(discriminator.state_dict(), os.path.join(args.checkpoint_dir, 'disc_{}'.format(epoch)))
+        torch.save(generator.state_dict(), os.path.join(args.checkpoint_dir, 'gen_{}'.format(epoch)))
