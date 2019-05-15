90f620e886089e70f368d1463421c38a5011c8e7
baris <baris4111@gmail.com>
2019-05-15 10:11:31 +0300

    self_attention added

--- model.py
+++ model.py
@@ -21,12 +21,13 @@ class Interpolate(nn.Module):
 
 
 class SeparableConvTranspose2d(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, mid_channels):
         super(SeparableConvTranspose2d, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.conv1 = nn.Conv2d(in_channels, in_channels, (in_channels, 1), stride=1, padding=(1, 0))
-        self.conv2 = nn.Conv2d(in_channels, out_channels, (1, in_channels), stride=1, padding=(0, 1))
+        self.mid_channels = mid_channels
+        self.conv1 = nn.Conv2d(in_channels, mid_channels, (in_channels, 1), stride=1, padding=(1, 0))
+        self.conv2 = nn.Conv2d(mid_channels, out_channels, (1, in_channels), stride=1, padding=(0, 1))
 
     def forward(self, x):
         u1 = torch.zeros((self.in_channels * 2, self.in_channels))
@@ -34,9 +35,9 @@ class SeparableConvTranspose2d(nn.Module):
         u1 = u1.reshape(1, 1, u1.shape[0], u1.shape[1])
         res_conv1 = self.conv1(u1)
 
-        u2 = torch.zeros((1, 512, self.in_channels * 2, self.in_channels * 2))
+        u2 = torch.zeros((1, self.mid_channels, self.in_channels * 2, self.in_channels * 2))
         u2[:, :, ::2] = res_conv1.permute(0, 1, 3, 2)
-        u2 = u2.permute(0, 1, 3, 2).reshape(1, 512, u2.shape[2], u2.shape[3])
+        u2 = u2.permute(0, 1, 3, 2).reshape(1, self.mid_channels, u2.shape[2], u2.shape[3])
         res_conv2 = self.conv2(u2)
 
         return res_conv2
---
+++ self_attention.py
@@ -0,0 +1,48 @@
+from torch import nn
+import torch.nn.functional as F
+import torch
+from spectral_normalization import SpectralNorm
+
+
+class SelfAttentionPost(nn.Module):
+    def __init__(self, input_size, attention_size):
+        super(SelfAttentionPost, self).__init__()
+        self.attention_size = attention_size
+        self.gamma = nn.Parameter(torch.tensor(0.))
+        self.h = SpectralNorm(nn.Conv2d(input_size, self.attention_size, 1, stride=1))
+        self.i = SpectralNorm(nn.Conv2d(self.attention_size, input_size, 1, stride=1))
+
+    def forward(self, x, att):
+        width = x.size(2)
+        height = x.size(3)
+        m = x
+        h = self.gamma * self.h(m)
+        h = h.permute(0, 2, 3, 1).contiguous().view(-1, width * height, self.attention_size)
+        h = torch.bmm(att, h)
+        h = h.view(-1, width, height, self.attention_size).permute(0, 3, 1, 2)
+        m = self.i(h) + m
+        return m
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, input_size, attention_size):
+        super(SelfAttention, self).__init__()
+        self.attention_size = attention_size
+
+        # attention layers
+        self.f = SpectralNorm(nn.Conv2d(input_size, attention_size, 1, stride=1))
+        self.g = SpectralNorm(nn.Conv2d(input_size, attention_size, 1, stride=1))
+        self.input_size = input_size
+
+    def forward(self, x):
+        width = x.size(2)
+        height = x.size(3)
+        channels = x.size(1)
+        m = x
+        f = self.f(m)
+        f = torch.transpose(f.view(-1, self.attention_size, width * height), 1, 2)
+        g = self.g(m)
+        g = g.view(-1, self.attention_size, width * height)
+        att = torch.bmm(f, g)
+
+        return F.softmax(att, 1)
